199fcbcf521c4433605d7e4244a9dfbd00eec78b
stalker9855 <andrey.l.evseev@gmail.com>
2024-03-05 15:26:12 +0200

    Revert "http"
    This reverts commit 712970ea7944146ad14c0bc124197a9951508986.

--- WebApplication2/Models/LocalAnimeModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebApplication2.Models
-{
-    public class LocalAnimeModel
-    {
-        public int Id { get; set; }
-        public string? Name { get; set; }
-    }
-}
--- WebApplication2/Services/ApiService/IApiService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WebApplication2.Services.ApiService
-{
-    public interface IApiService
-    {
-        public Task<T> GetApi<T>(string? url);
-    }
-}
