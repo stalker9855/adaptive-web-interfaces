712970ea7944146ad14c0bc124197a9951508986
stalker9855 <andrey.l.evseev@gmail.com>
2024-03-05 15:24:23 +0200

    http

---
+++ WebApplication2/Program.cs
@@ -0,0 +1,26 @@
+using WebApplication2.Services.ApiService;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpClient();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddTransient<IApiService, ApiService>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ WebApplication2/Services/ApiService/ApiService.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApplication2.Services.ApiService
+{
+    public class ApiService : IApiService
+    {
+        readonly private HttpClient _httpClient;
+        public ApiService(HttpClient httpClient) 
+        { 
+            _httpClient = httpClient;
+        }
+        public async Task<T?> GetApi<T>(string? url)
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync(url); 
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<T>();
+        }
+    }
+}
